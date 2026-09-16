fd82bc45ff88f544bc0d57e4edd9a448cfb8b286
Shravanthi <147586547+Shravanthi20@users.noreply.github.com>
2026-09-16 13:08:19 +0530

    Use connection method to access Snowflake session
    Replace get_active_session with connection method to access Snowflake session.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 st.title("Customize Your Smoothie! :cup_with_straw:")
-
-session = get_active_session()
+cnx= st.connection("snowflake")
+session = cnx.session()
 
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(
     col("FRUIT_NAME")
